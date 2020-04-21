325ca76a6df4618e9e259f95e2626fc3c36f2c34
Margaret Orton <margaret@hitmixer.com>
2020-04-21 10:25:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-
 var express = require('express');
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 var app = express();
 
 app.get('/', function(req, res){
